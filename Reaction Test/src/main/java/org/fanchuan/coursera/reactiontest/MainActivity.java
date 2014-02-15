5a71ea9c812e8f59db865f22808fcc65118dbde0
David Monk <velowander@users.noreply.github.com>
2014-02-15 22:37:03 +0800

    Functionality unchanged Internal refactor: create onClickListener and ReactionTime observer (support library) fragment - static inner class StatusAreaFragment. This fragment is now responsible for hosting the ReactionTimer instance and updating the reaction time and score. It also listens to the Main Activity's button. The main activity doesn't do much at this point. The purpose of the refactor is to learn more about the fragment lifecycle and (next step) setRetainInstance to true and allow orientation changes to see if the test continues normally.

--- Reaction Test/src/main/java/org/fanchuan/coursera/reactiontest/MainActivity.java
+++ Reaction Test/src/main/java/org/fanchuan/coursera/reactiontest/MainActivity.java
@@ -1,5 +1,6 @@
 package org.fanchuan.coursera.reactiontest;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -8,39 +9,28 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity implements ReactionTimerObserver {
+public class MainActivity extends ActionBarActivity {
 
     final String TAG = MainActivity.class.getSimpleName();
-    //Persistent items saved to preferences; currently best reaction time only
-    private ReactionTimer reactionTimer;
-    private SharedPreferences prefs;
-    private long bestTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Retrieve preferences (saved best reaction time)
-        try {
-            prefs = PreferenceManager.getDefaultSharedPreferences(this);
-            bestTime = prefs.getLong(getResources().getString(R.string.keyBestTime), 10000); //default reaction time if not saved
-            Log.d(TAG, "bestTime: " + bestTime);
-            showBestTime();
-        } catch (Exception e) {
-            Log.e(TAG, "Unable to get best time", e);
-        }
-
-        //Each state has its own unique text description
-        this.reactionTimer = new ReactionTimer(this, getResources().getStringArray(R.array.state_descriptions));
+        StatusAreaFragment statusAreaFragment = (StatusAreaFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_status_area);
+        findViewById(R.id.go_button).setOnClickListener(statusAreaFragment);
 
         /*
         // Create the adapter that will return a fragment for each of the three
@@ -59,21 +49,8 @@ public class MainActivity extends ActionBarActivity implements ReactionTimerObse
         // Inflate the menu; this adds items to the action bar if it is present.
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main, menu);
+        Log.d(TAG, "Options menu inflated");
         return true;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //Check preferences to see if user wants to remove best time
-        if (prefs.getBoolean(getResources().getString(R.string.keyClearBest), false)) {
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.remove(getResources().getString(R.string.keyBestTime));
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD)
-                editor.apply();
-            else
-                editor.commit();
-        }
     }
 
     @Override
@@ -95,64 +72,125 @@ public class MainActivity extends ActionBarActivity implements ReactionTimerObse
         return super.onOptionsItemSelected(item);
     }
 
-    public void showEndTestNotification(String reactionTimeText) {
-        /* Generates a sample notification, notifying of the reaction time.
-        Worked in several Genymotion API18 emulators and Moto Xoom tablet but threw Exception on LG API10 phone (??) */
-        try {
-            NotificationCompat.Builder notifyReactionTimeBuilder = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.ic_launcher)
-                    .setContentText(reactionTimeText)
-                    .setContentTitle(this.getString(R.string.app_name));
-            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            mNotificationManager.notify(0, notifyReactionTimeBuilder.build());
-        } catch (Exception e) {
-            Log.w(TAG, "Unable to display score notification", e);
-        }
-    }
+    public static class StatusAreaFragment extends Fragment implements ReactionTimerObserver, View.OnClickListener {
 
-    private void showBestTime() {
-        // If test complete, run submitLatestTime() first; Assumes bestTime is already populated
-        TextView vw = (TextView) findViewById(R.id.bestTime);
-        String bestTimeText = String.format(getResources().getString(R.string.bestTime), bestTime);
-        vw.setText(bestTimeText);
-    }
+        private final String TAG = StatusAreaFragment.class.getSimpleName();
 
-    public void submitLatestTime(long latestTime, String reactionTimeText) {
-        /* Check latestTime versus bestTime, if so, update bestTime and persist in preferences
-            Check Shared Preferences: does user wants a notification displayed?
-         */
-        if (latestTime < bestTime) {
-            bestTime = latestTime;
-            Log.d(TAG, "submitLatestTime: " + latestTime);
+        //Persistent items saved to shared preferences (best time, and settings)
+        private ReactionTimer reactionTimer;
+        private SharedPreferences prefs;
+        private long bestTime;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            //Each state has its own unique text description
+            this.reactionTimer = new ReactionTimer(this, getResources().getStringArray(R.array.state_descriptions));
+
+            //Retrieve preferences (saved best reaction time)
             try {
+                prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                bestTime = prefs.getLong(getResources().getString(R.string.keyBestTime), 10000); //default reaction time if not saved
+                Log.d(TAG, "bestTime: " + bestTime);
+                //We can't call showBestTime() yet because root View isn't inflated until onCreateView();
+            } catch (Exception e) {
+                Log.e(TAG, "Unable to get best time", e);
+            }
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_status_area, container, false);
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            showBestTime();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            //Check preferences to see if user wants to remove best time
+            if (prefs.getBoolean(getResources().getString(R.string.keyClearBest), false)) {
                 SharedPreferences.Editor editor = prefs.edit();
-                editor.putLong(getResources().getString(R.string.keyBestTime), latestTime);
+                editor.remove(getResources().getString(R.string.keyBestTime));
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD)
                     editor.apply();
                 else
                     editor.commit();
-            } catch (Exception e) {
-                Log.e(TAG, "Unable to save best time", e);
             }
-            Log.i(TAG, "Committed to preferences latestTime: " + latestTime);
-            showBestTime();
         }
-        //If notification setting is on, displays whether latestTime is bestTime or not
-        String keyNotification = getResources().getString(R.string.keyNotification);
-        boolean settingNotification = prefs.getBoolean(keyNotification, false);
-        Log.d(TAG, keyNotification + ": " + String.valueOf(settingNotification));
-        if (settingNotification)
-            showEndTestNotification(reactionTimeText);
-    }
 
-    public synchronized void updateUiStatus(String statusText) {
-        final TextView VW_STATUS = (TextView) findViewById(R.id.status);
-        VW_STATUS.setText(statusText);
-    }
+        @SuppressWarnings("unused")
+        public void onClick(View vw) {
+            reactionTimer.click();
+        }
 
-    @SuppressWarnings("unused")
-    public void clickGoButton(View vw) {
-        reactionTimer.click();
-    }
+        private void showBestTime() {
+            /* Cannot call before root View is inflated
+            Shows the best time stored in member var bestTime in the status area, after formatting.
+            */
+            try {
+                TextView vw = (TextView) getView().findViewById(R.id.bestTime);
+                String bestTimeText = String.format(getResources().getString(R.string.bestTime), bestTime);
+                vw.setText(bestTimeText);
+            } catch (Exception e) {
+                Log.e(TAG, "Unable to display bestTime in status area", e);
+            }
+        }
 
+        public void showEndTestNotification(String reactionTimeText) {
+        /* Generates a sample notification, notifying of the reaction time.
+        Worked in several Genymotion API18 emulators and Moto Xoom tablet but threw Exception on LG API10 phone (??) */
+            try {
+                Activity parentActivity = getActivity();
+                NotificationCompat.Builder notifyReactionTimeBuilder = new NotificationCompat.Builder(parentActivity)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentText(reactionTimeText)
+                        .setContentTitle(this.getString(R.string.app_name));
+                NotificationManager mNotificationManager = (NotificationManager) parentActivity.getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(0, notifyReactionTimeBuilder.build());
+            } catch (Exception e) {
+                Log.w(TAG, "Unable to display score notification", e);
+            }
+        }
+
+        public void submitLatestTime(long latestTime, String reactionTimeText) {
+        /* Check latestTime versus bestTime, if so, update bestTime and persist in preferences
+            Check Shared Preferences: does user wants a notification displayed?
+         */
+            if (latestTime < bestTime) {
+                bestTime = latestTime;
+                Log.d(TAG, "submitLatestTime: " + latestTime);
+                try {
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putLong(getResources().getString(R.string.keyBestTime), latestTime);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD)
+                        editor.apply();
+                    else
+                        editor.commit();
+                } catch (Exception e) {
+                    Log.e(TAG, "Unable to save best time", e);
+                }
+                Log.i(TAG, "Committed to preferences latestTime: " + latestTime);
+                showBestTime();
+            }
+            //If notification setting is on, displays whether latestTime is bestTime or not
+            String keyNotification = getResources().getString(R.string.keyNotification);
+            boolean settingNotification = prefs.getBoolean(keyNotification, false);
+            Log.d(TAG, keyNotification + ": " + String.valueOf(settingNotification));
+            if (settingNotification)
+                showEndTestNotification(reactionTimeText);
+        }
+
+        public synchronized void updateUiStatus(String statusText) {
+            final TextView VW_STATUS = (TextView) getView().findViewById(R.id.status);
+            VW_STATUS.setText(statusText);
+        }
+    }
 }
